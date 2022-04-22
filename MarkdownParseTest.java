b8a9f58e76c34c50b40cb5c0b233bb6e2151a171
Joshjppark <joshjppark@gmail.com>
2022-04-21 17:58:00 -0700

    more testing

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,11 @@
 import static org.junit.Assert.*;
+import org.junit.*;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
-import org.junit.*;
+
 
 
 public class MarkdownParseTest {
@@ -15,9 +16,117 @@ public class MarkdownParseTest {
 
 
     @Test
-    public void testgetLinks() {
+    public void testbasegetLinks() {
         try {
             assertEquals(List.of("https://something.com", "some-thing.html"), MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))));
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Test
+    public void test1getLinks() {
+        try {
+            assertEquals(List.of("https://www.youtube.com/"), MarkdownParse.getLinks(Files.readString(Path.of("test1.md"))));
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Test
+    public void test2getLinks() {
+        try {
+            assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(Path.of("test2.md"))));
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Test
+    public void test3getLinks() {
+        try {
+            assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(Path.of("test3.md"))));
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Test
+    public void test4getLinks() {
+        try {
+            assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(Path.of("test4.md"))));
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Test
+    public void test5getLinks() {
+        try {
+            assertEquals(List.of("hi.com"), MarkdownParse.getLinks(Files.readString(Path.of("test5.md"))));
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Test
+    public void test_file2getLinks() {
+        try {
+            assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(Files.readString(Path.of("test-file2.md"))));
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+    
+    @Test
+    public void test_file3getLinks() {
+        try {
+            assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(Path.of("test-file3.md"))));
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Test
+    public void test_file4getLinks() {
+        try {
+            assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(Path.of("test-file4.md"))));
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Test
+    public void test_file5getLinks() {
+        try {
+            assertEquals(List.of("page.com"), MarkdownParse.getLinks(Files.readString(Path.of("test-file5.md"))));
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Test
+    public void test_file6getLinks() {
+        try {
+            assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(Path.of("test-file6.md"))));
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Test
+    public void test_file7getLinks() {
+        try {
+            assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(Path.of("test-file7.md"))));
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Test
+    public void test_file8getLinks() {
+        try {
+            assertEquals(List.of(), MarkdownParse.getLinks(Files.readString(Path.of("test-file8.md"))));
         } catch (Exception e) {
             throw new IllegalArgumentException();
         }
